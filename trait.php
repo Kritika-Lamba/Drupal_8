3904a2fb7463ce35d370a1570d00563973d69d50
Kritika-Lamba <66401724+Kritika-Lamba@users.noreply.github.com>
2020-06-20 18:01:44 +0530

    Assignment 5 june

---
+++ trait.php
@@ -0,0 +1,15 @@
+<?php
+trait message1 {
+  public function msg1() {
+    echo "page  is loading with traits "; 
+  }
+}
+
+class Welcome {
+  use message1;
+}
+
+$obj = new Welcome();
+$obj->msg1();
+?>
+© 2020 GitHub, Inc.
